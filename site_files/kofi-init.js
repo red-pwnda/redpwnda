f0bf348eb5032ea0cbd3e519865f6b01cc5f63c2
Kuldeep Singh <kskulhari007@gmail.com>
2024-12-06 18:20:15 +0530

    added and updated contacts, index with footer

---
+++ site_files/kofi-init.js
@@ -0,0 +1,6 @@
+kofiWidgetOverlay.draw('redpwnda', {
+    'type': 'floating-chat',
+    'floating-chat.donateButton.text': 'Support Me',
+    'floating-chat.donateButton.background-color': '#ff38b8',
+    'floating-chat.donateButton.text-color': '#fff'
+  });
